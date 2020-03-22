2c8a7241328ce6c92ca42b180aaf6f76ea3f2da0
Arrow <light.shaft.1@gmail.com>
2020-03-22 19:59:42 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
+  let array = bracketsConfig.map(i=>i.join(''))
+  let answ = str;
+  let tr = [true];
+  while (tr.includes(true) && answ.length){
+      for (let i = 0; i < array.length; i++ ) {
+          answ = answ.replace(array[i],'');
+      }
+      tr = array.map( p=> {
+          if (answ.indexOf(p) < 0) {
+              return false;
+          } else {
+              return true; }
+      })
+  }
+
+  return !answ;
+
+
+
   // your solution
 }
